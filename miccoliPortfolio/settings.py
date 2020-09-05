2113ab3c823ca528533b3055ed0073b515d4dd59
Sandro Miccoli <smiccoli@gmail.com>
2020-09-05 17:02:21 -0300

    Updating settings

--- miccoliPortfolio/settings.py
+++ miccoliPortfolio/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'miccoliPortfolio',
-    'storages',
-    'boto',
+    # 'storages',
+    # 'boto',
 ]
 
 MIDDLEWARE = [
