c69a23cf78cdb483512c7a568c2a5273f915555b
Sandro Miccoli <smiccoli@gmail.com>
2020-09-05 16:35:14 -0300

    Revert "Webflow beta - Settings"
    This reverts commit 3ccc70e19d2ff7777e2eab82c6d9c721688c4f75.

--- miccoliPortfolio/settings.py
+++ miccoliPortfolio/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = ')oris5r%6&q4h4%0d$v*a+up4m5af%jv3x3o96e077h@n+6yzw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sandromiccoli.com','site-portfolio-sandromiccoli.herokuapp.com/']
-# ALLOWED_HOSTS = ['127.0.0.1:8000','localhost']
+ALLOWED_HOSTS = ['127.0.0.1:8000','sandromiccoli.com','*','localhost']
 
 
 # Application definition
