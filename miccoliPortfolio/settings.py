7da147911f015d72ba129f14b98cfcb44a7a84fb
Sandro Miccoli <smiccoli@gmail.com>
2020-09-08 19:37:23 -0300

    Adding S3 settings

--- miccoliPortfolio/settings.py
+++ miccoliPortfolio/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')oris5r%6&q4h4%0d$v*a+up4m5af%jv3x3o96e077h@n+6yzw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','sandromiccoli.com','*','localhost']
+ALLOWED_HOSTS = ['sandromiccoli.com','site-portfolio-sandromiccoli.herokuapp.com']
+# ALLOWED_HOSTS = ['127.0.0.1:8000','localhost',]
 
 
 # Application definition
@@ -40,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'miccoliPortfolio',
-    # 'storages',
-    # 'boto',
+    'storages',
+    'boto',
 ]
 
 MIDDLEWARE = [
@@ -137,11 +138,11 @@ MEDIA_URL = 'media/'
 django_heroku.settings(locals())
 
 # Storage on S3 settings are stored as os.environs to keep settings.py clean
-# if not DEBUG:
-#    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-#    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
-#    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-#    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
-#    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-#    STATIC_URL = S3_URL
-#    MEDIA_URL = f'http://portfolio-sandromiccoli.s3-website-sa-east-1.amazonaws.com'
+if not DEBUG:
+   AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+   AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
+   AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+   STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+   S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+   STATIC_URL = S3_URL
+   MEDIA_URL = f'http://portfolio-sandromiccoli.s3-website-sa-east-1.amazonaws.com'
