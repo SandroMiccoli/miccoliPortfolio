73057887bd3b873592496a037c0d6c81a5e95314
Sandro Miccoli <smiccoli@gmail.com>
2020-09-08 22:27:33 -0300

    Changing STATIC_ROOT

--- miccoliPortfolio/settings.py
+++ miccoliPortfolio/settings.py
@@ -145,7 +145,7 @@ else:
    AWS_LOCATION = 'static'   
    S3_URL = f'https://%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, AWS_LOCATION)
    STATIC_URL = S3_URL
-   STATIC_ROOT = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+   STATIC_ROOT = S3_URL
    # MEDIA_URL = 'http://portfolio-sandromiccoli.s3-website-sa-east-1.amazonaws.com/'
    AWS_MEDIA_LOCATION = 'media/'
    MEDIA_URL = 'https://%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, AWS_MEDIA_LOCATION)
