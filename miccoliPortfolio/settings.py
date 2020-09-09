1e3db66898be256b67948c548dfd145e13a1afa2
Sandro Miccoli <smiccoli@gmail.com>
2020-09-08 22:10:22 -0300

    Using storage.py

--- miccoliPortfolio/settings.py
+++ miccoliPortfolio/settings.py
@@ -150,7 +150,7 @@ else:
    AWS_MEDIA_LOCATION = 'media/'
    MEDIA_URL = 'https://%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, AWS_MEDIA_LOCATION)
    MEDIA_ROOT = MEDIA_URL
-   # STATICFILES_STORAGE = 'miccoliPortfolio.storage.AmazonS3'
-   STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+   STATICFILES_STORAGE = 'miccoliPortfolio.storage.AmazonS3'
+   # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
 
 django_heroku.settings(locals(), staticfiles=False)
