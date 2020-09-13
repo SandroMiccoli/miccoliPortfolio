7c339845a2f6c7e1a7df97350783caa862ebf5e0
Sandro Miccoli <smiccoli@gmail.com>
2020-09-13 19:39:51 -0300

    Adding videos to projects

--- miccoliPortfolio/admin.py
+++ miccoliPortfolio/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Project, Image, Credit
+from .models import Project, Image, Credit, Video
 
 
 class ImageInline(admin.TabularInline):
@@ -9,11 +9,15 @@ class ImageInline(admin.TabularInline):
 class CreditInline(admin.TabularInline):
 	model = Credit
 
+class VideoInline(admin.TabularInline):
+	model = Video
+
 class ProjectAdmin(admin.ModelAdmin):
 	list_display = ('title', 'date')
 	inlines = [
 		ImageInline,
-		CreditInline
+		CreditInline,
+		VideoInline
 	]
 
 admin.site.register(Project, ProjectAdmin)
